106ccf2dfafaea61e658c006d025536d42fb22ef
BenwCr <bwcrouse15@gmail.com>
2024-08-17 21:02:15 -0500

    char_count added

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ book_path = "books/frankenstein.txt"
 
 def main():
     text = book_text(book_path)
-    wordcount = word_count(text)
-    print(f"{wordcount} words found in the document")
+    wordCount = word_count(text)
+    charDict = char_count(text)
+    print(f"{wordCount} words found in the document\n{charDict}")
 
 def book_text(path):
     with open(path) as f:
@@ -13,6 +14,19 @@ def book_text(path):
 def word_count(text):
     list=text.split()
     return(len(list))
+
+def char_count(text):
+    loweredCase = text.lower()
+    dictResult = {}
+    for char in loweredCase:
+        count = 0
+        if char not in dictResult:
+            dictResult[char] = 1
+        else:
+            dictResult[char] += 1
+
+
+    return dictResult
 
 
 main ()
